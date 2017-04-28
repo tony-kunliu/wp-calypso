5663fd112533f155478ae47f01523d9963e9c412
Jon Surrell <jon.surrell@automattic.com>
2017-04-28 17:32:01 +0200

    Return getNoticeProps -> getNotice
    Return React element rather than props.

--- client/signup/jetpack-connect/jetpack-connect-notices.jsx
+++ client/signup/jetpack-connect/jetpack-connect-notices.jsx
@@ -32,14 +32,14 @@ class JetpackConnectNotices extends Component {
 		onDismissClick: PropTypes.func,
 	};
 
-	getNoticeProps() {
+	getNotice() {
 		const {
 			noticeType,
 			onDismissClick,
 			translate,
 		} = this.props;
 
-		const noticeProps = {
+		const noticeDefaults = {
 			icon: 'notice',
 			onDismissClick,
 			showDismiss: !! onDismissClick,
@@ -49,93 +49,151 @@ class JetpackConnectNotices extends Component {
 
 		switch ( noticeType ) {
 			case 'notExists':
-				return noticeProps;
+				return (
+					<Notice { ...noticeDefaults } />
+				);
 
 			case 'isDotCom':
-				noticeProps.icon = 'block';
-				noticeProps.text = translate( 'That\'s a WordPress.com site, so you don\'t need to connect it.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="block"
+						text={ translate( 'That\'s a WordPress.com site, so you don\'t need to connect it.' ) }
+					/>
+				);
 
 			case 'notWordPress':
-				noticeProps.icon = 'block';
-				noticeProps.text = translate( 'That\'s not a WordPress site.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="block"
+						text={ translate( 'That\'s not a WordPress site.' ) }
+					/>
+				);
 
 			case 'notActiveJetpack':
-				noticeProps.icon = 'block';
-				noticeProps.text = translate( 'Jetpack is deactivated.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="block"
+						text={ translate( 'Jetpack is deactivated.' ) }
+					/>
+				);
 
 			case 'outdatedJetpack':
-				noticeProps.icon = 'block';
-				noticeProps.text = translate( 'You must update Jetpack before connecting.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="block"
+						text={ translate( 'You must update Jetpack before connecting.' ) }
+					/>
+				);
 
 			case 'notConnectedJetpack':
-				noticeProps.icon = 'link-break';
-				noticeProps.text = translate( 'Jetpack is currently disconnected.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="link-break"
+						text={ translate( 'Jetpack is currently disconnected.' ) }
+					/>
+				);
 
 			case 'alreadyOwned':
-				noticeProps.icon = 'plugins';
-				noticeProps.status = 'is-success';
-				noticeProps.text = translate( 'Jetpack is connected.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="plugins"
+						status="is-success"
+						text={ translate( 'Jetpack is connected.' ) }
+					/>
+				);
 
 			case 'notJetpack':
-				noticeProps.icon = 'status';
-				noticeProps.status = 'is-noticeType';
-				noticeProps.text = translate( 'Jetpack couldn\'t be found.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="status"
+						status="is-noticeType"
+						text={ translate( 'Jetpack couldn\'t be found.' ) }
+					/>
+				);
 
 			case 'alreadyConnected':
-				noticeProps.icon = 'status';
-				noticeProps.status = 'is-success';
-				noticeProps.text = translate( 'This site is already connected!' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="status"
+						status="is-success"
+						text={ translate( 'This site is already connected!' ) }
+					/>
+				);
 
 			case 'wordpress.com':
-				noticeProps.icon = 'status';
-				noticeProps.status = 'is-warning';
-				noticeProps.text = translate( 'Oops, that\'s us.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						icon="status"
+						status="is-warning"
+						text={ translate( 'Oops, that\'s us.' ) }
+					/>
+				);
 
 			case 'retryingAuth':
-				noticeProps.status = 'is-warning';
-				noticeProps.text = translate( 'Error authorizing. Page is refreshing for an other attempt.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						status="is-warning"
+						text={ translate( 'Error authorizing. Page is refreshing for an other attempt.' ) }
+					/>
+				);
 
 			case 'retryAuth':
-				noticeProps.status = 'is-warning';
-				noticeProps.text = translate( 'In some cases, authorization can take a few attempts. Please try again.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						status="is-warning"
+						text={ translate( 'In some cases, authorization can take a few attempts. Please try again.' ) }
+						/>
+				);
 
 			case 'secretExpired':
-				noticeProps.text = translate( 'Oops, that took a while. You\'ll have to try again.' );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						text={ translate( 'Oops, that took a while. You\'ll have to try again.' ) }
+					/>
+				);
 
 			case 'defaultAuthorizeError':
-				noticeProps.text = translate( 'Error authorizing your site. Please {{link}}contact support{{/link}}.', {
-					components: { link: <a href="https://jetpack.com/contact-support" target="_blank" rel="noopener noreferrer" /> }
-				} );
-				return noticeProps;
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						text={ translate( 'Error authorizing your site. Please {{link}}contact support{{/link}}.', {
+							components: { link: <a href="https://jetpack.com/contact-support" target="_blank" rel="noopener noreferrer" /> }
+						} ) }
+					/>
+				);
 
 			case 'alreadyConnectedByOtherUser':
-				noticeProps.status = 'is-warning';
-				noticeProps.text = translate(
-					'This site is already connected to a different WordPress.com user, ' +
-					'you need to disconnect that user before you can connect another.'
+				return (
+					<Notice
+						{ ...noticeDefaults }
+						status="is-warning"
+						text={ translate(
+							'This site is already connected to a different WordPress.com user, ' +
+							'you need to disconnect that user before you can connect another.'
+						) }
+					/>
 				);
-				return noticeProps;
 		}
 	}
 
 	render() {
-		const noticeProps = this.getNoticeProps();
-		if ( noticeProps ) {
+		const notice = this.getNotice();
+		if ( notice ) {
 			return (
 				<div className="jetpack-connect__notices-container">
-					<Notice { ...noticeProps } />
+					{ notice }
 				</div>
 			);
 		}
