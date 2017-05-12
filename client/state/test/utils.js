b2a61596f0fe76c37b83a0d7b5e54baf214539ed
Dennis Snell <dennis.snell@automattic.com>
2017-05-12 16:40:19 -0500

    Remove "only" from tests

--- client/state/test/utils.js
+++ client/state/test/utils.js
@@ -274,7 +274,7 @@ describe( 'utils', () => {
 		} );
 	} );
 
-	describe.only( '#keyedReducer', () => {
+	describe( '#keyedReducer', () => {
 		const grow = name => ( { type: 'GROW', name } );
 
 		const age = ( state = 0, action ) =>
