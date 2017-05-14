23117a34b0928162358ff1c09921cd9eaa6dc640
Hew <github@mhs.fastmail.com>
2017-05-13 20:59:41 -0400

    Domain management: initial contact details Redux logic
    We’re going to unify and Reduxify the checkout and management contact details forms. WIP, pardon the dust.

--- client/state/domains/reducer.js
+++ client/state/domains/reducer.js
@@ -6,8 +6,10 @@ import { combineReducers } from 'redux';
 /**
  * Internal dependencies
  */
+import management from './management/reducer';
 import suggestions from './suggestions/reducer';
 
 export default combineReducers( {
+	management
 	suggestions
 } );
